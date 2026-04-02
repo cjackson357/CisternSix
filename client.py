5a3bf1e89c01d833f42fe46acaac7989679d00d2
kevinmoralesrom <kevinmoralesrom@gmail.com>
2026-04-02 15:40:09 -0400

    Update client.py
    - Added 4 camera streams
    - Added fixes to reduce latency

--- client.py
+++ client.py
@@ -1,27 +1,109 @@
 import socket
 import keyboard
 import time
+import cv2
+import numpy as np
+import threading
 
-PI_IP = "192.168.1.2"   # CHANGE to your Pi's IP
+PI_IP = "192.168.1.3"
 PORT = 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((PI_IP, PORT))
+# -------------------------------
+# 🔹 SOCKET CONTROL THREAD
+# -------------------------------
+def control_thread():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((PI_IP, PORT))
+    print("Connected to Pi (control)")
 
+    try:
+        while True:
+            w = int(keyboard.is_pressed("w"))
+            a = int(keyboard.is_pressed("a"))
+            s = int(keyboard.is_pressed("s"))
+            d = int(keyboard.is_pressed("d"))
+
+            message = f"{w},{a},{s},{d}\n"
+            sock.sendall(message.encode())
+
+            time.sleep(0.01)
+
+    except:
+        sock.close()
+
+
+# -------------------------------
+# 🔹 CAMERA THREAD CLASS
+# -------------------------------
+class CameraStream:
+    def __init__(self, url):
+        self.cap = cv2.VideoCapture(url)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        self.frame = np.zeros((240, 320, 3), dtype=np.uint8)
+        self.running = True
+
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        while self.running:
+            if self.cap.isOpened():
+                self.cap.grab()  
+                ret, frame = self.cap.read()
+                if ret:
+                    self.frame = cv2.resize(frame, (320, 240))
+
+    def get_frame(self):
+        return self.frame
+
+    def stop(self):
+        self.running = False
+        self.cap.release()
+
+
+# -------------------------------
+# 🔹 START EVERYTHING
+# -------------------------------
+
+# Camera URLs
+urls = [
+    f"http://{PI_IP}:8080/?action=stream",
+    f"http://{PI_IP}:8081/?action=stream",
+    f"http://{PI_IP}:8082/?action=stream",
+    f"http://{PI_IP}:8083/?action=stream"
+]
+
+# Start control thread
+threading.Thread(target=control_thread, daemon=True).start()
+
+# Start camera streams
+cams = [CameraStream(url) for url in urls]
+
+print("All camera threads started")
+
+# -------------------------------
+# 🔹 DISPLAY LOOP (MAIN THREAD)
+# -------------------------------
 try:
-
     while True:
 
-        w = int(keyboard.is_pressed("w"))
-        a = int(keyboard.is_pressed("a"))
-        s = int(keyboard.is_pressed("s"))
-        d = int(keyboard.is_pressed("d"))
+        frames = [cam.get_frame() for cam in cams]
 
-        message = f"{w},{a},{s},{d}\n"
+        # Combine into 2x2 grid
+        top = cv2.hconcat([frames[0], frames[1]])
+        bottom = cv2.hconcat([frames[2], frames[3]])
+        grid = cv2.vconcat([top, bottom])
 
-        sock.sendall(message.encode())
+        cv2.imshow("ROV Camera System", grid)
 
-        time.sleep(0.02)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 except KeyboardInterrupt:
-    sock.close()
+    pass
+
+finally:
+    for cam in cams:
+        cam.stop()
+
+    cv2.destroyAllWindows()
